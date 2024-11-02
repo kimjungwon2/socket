042e6d405863d596b7d5a3a9a63ea07649092d4c
jungwon <mac2001035@gmail.com>
2024-11-02 22:42:16 +0900

    REFACTOR: 최상단 파일 삭제(2)

---
+++ src/test/java/org/practice/socket/SocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
